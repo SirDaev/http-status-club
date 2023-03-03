9fc5ea4d452e94ae6cdb079d3a426052dc84d751
Dave Judd <52185dj@gmail.com>
2023-03-03 15:56:41 -0600

    Create status code handler

---
+++ netlify/functions/statusCodeHandler.js
@@ -0,0 +1,22 @@
+exports.handler = async function (event, context) {
+
+  const code = event.path.match(/([^\/]*)\/*$/)[1];
+
+  return {
+    statusCode: parseInt(code),
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(
+      [
+        {
+          message: "You got the code!",
+          code: code
+        }
+      ]
+    ),
+  };
+}
