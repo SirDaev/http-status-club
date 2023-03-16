eaa276b589ab1149faa9eb2bc507203ede20def3
Dave Judd <52185dj@gmail.com>
2023-03-16 18:33:24 -0500

    Add Tailwind

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const pug = require('pug');
 const data = require('./data.json');
 
-const compiledFunction = pug.compileFile('./index.pug');
+const compiledFunction = pug.compileFile('src/index.pug');
 
 const html = compiledFunction({data: data});
 
-const stream = fs.createWriteStream('./index.html');
+const stream = fs.createWriteStream('index.html');
 
 stream.once('open', (fd) => stream.end(html));
