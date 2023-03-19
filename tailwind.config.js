e13c809d553b7b073ccad9b702aed43d0999c88e
Dave Judd <52185dj@gmail.com>
2023-03-19 12:03:33 -0500

    Add basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    colors: {
+      'black': '#292929',
+      'green': '#71C597',
+      'green-lite': '#aacdb9',
+      'white': '#EEEEEE'
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px'
+      },
+    },
+    fontFamily: {
+      sans: ['Maven Pro', 'sans-serif'],
+      serif: ['Rokkitt', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
